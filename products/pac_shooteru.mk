296a2af08ec89aac2faefb8c7f48051e03ffae39
thekraven <savethestyle1@gmail.com>
2013-03-29 23:44:47 -0500

    fix
    Change-Id: I98d326aacc44e3e73ba4d950560d9c5a6983c66f

--- products/pac_shooteru.mk
+++ products/pac_shooteru.mk
@@ -6,6 +6,7 @@ OVERLAY_TARGET := pa_hdpi
 
 # AOKP device overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/aokp/common
+PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/device/$(TARGET_PRODUCT)
 
 # PAC device overlay
 $(shell cp -f vendor/pac/prebuilt/common/bootanimation_framework/android-logo-mask_htc-hdpi.png frameworks/base/core/res/assets/images/android-logo-mask.png)
